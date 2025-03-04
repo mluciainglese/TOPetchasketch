763e58313688a82adbc13d365d8e174da617c1bd
Lucia Inglese <152501487+mluciainglese@users.noreply.github.com>
2025-03-04 16:46:07 +0000

    create function to make grid size based on user input

--- js/main.js
+++ js/main.js
@@ -15,3 +15,29 @@ divContainer.addEventListener("mouseover", (event) => {
         event.target.style.backgroundColor = "rgb(186, 121, 255)"
     }
 })
+
+// Function to let user choose grid size
+
+document.querySelector("button").addEventListener("click", getUserInput)
+
+function getUserInput(){
+    let userInput = Number(document.querySelector("input").value)
+    
+    if (isNaN(userInput) || userInput < 1 || userInput > 100 || userInput % 1 != 0){
+        alert ("Please enter a number between 1 and 100")
+        return;
+    } else {
+        divContainer.innerHTML = ""
+
+        function createGrid(userInput){
+            for (let i = 0; i < userInput*userInput; i++) {
+                let newDiv = document.createElement("div")
+                newDiv.classList.add("square")
+                divContainer.appendChild(newDiv)
+            }       
+        }
+        
+        createGrid(userInput)
+    }
+
+}
